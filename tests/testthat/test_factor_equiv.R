a76110553577b0a8c069b2e9f616d72db56bcf38
Alexander Zwart <zwa005@csiro.au>
2017-12-06 15:44:07 +1100

    Convert factor level labels to letters

--- tests/testthat/test_factor_equiv.R
+++ tests/testthat/test_factor_equiv.R
@@ -7,7 +7,9 @@ context("Factor equivalence")
 ## Setup:
 ######################################################################
 
-f <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3))
+f <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
+            levels=1:3,
+            labels=LETTERS[1:3])
 
 ######################################################################
 ## Tests
@@ -23,10 +25,12 @@ test_that("Equivalence of identical factors",
             expect_that(fct_equiv(g,f,ordered=TRUE),is_true())  ## Commutative
           })
 
+
 g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
-            levels = 1:3,labels=3:1)
+            levels = 1:3,
+            labels=LETTERS[5:7])
 ##
-test_that("Different level labels, same order",
+test_that("Different labels, same level encoding, version 1",
           {
             expect_that(fct_equiv(f,g),is_true())
             expect_that(fct_equiv(g,f),is_true())  ## Commutative
@@ -34,10 +38,27 @@ test_that("Different level labels, same order",
             expect_that(fct_equiv(g,f,ordered=TRUE),is_true())  ## Commutative
           })
 
+
+## As before, but checking that fct_equiv is not confused by the
+## _same_ level labels being mapped in a different order!
 g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
-            levels = 3:1)
+            levels = 1:3,
+            labels=LETTERS[3:1])
 ##
-test_that("Same level labels, different order",
+test_that("Different labels, same level encoding, version 2",
+          {
+            expect_that(fct_equiv(f,g),is_true())
+            expect_that(fct_equiv(g,f),is_true())  ## Commutative
+            expect_that(fct_equiv(f,g,ordered=TRUE),is_true())
+            expect_that(fct_equiv(g,f,ordered=TRUE),is_true())  ## Commutative
+          })
+
+
+g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
+            levels = 3:1,
+            labels=LETTERS[3:1])
+##
+test_that("Same labels, different level encoding",
           {
             expect_that(fct_equiv(f,g),is_true())
             expect_that(fct_equiv(g,f),is_true())  ## Commutative
@@ -45,9 +66,12 @@ test_that("Same level labels, different order",
             expect_that(fct_equiv(g,f,ordered=TRUE),is_false()) ## Commutative
           })
 
-g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),levels=1:6)
+
+g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
+            levels=1:6,
+            labels=LETTERS[1:6])
 ##
-test_that("Extra levels, same level order",
+test_that("Unused levels, same level encoding",
           {
             expect_that(fct_equiv(f,g),is_true())
             expect_that(fct_equiv(g,f),is_true())  ## Commutative
@@ -65,9 +89,9 @@ test_that("Extra levels, same level order",
           })
 
 g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
-            levels = 1:6,labels=c(1:3,4:6))
+            levels = 1:6,labels=LETTERS[c(7:12)])
 ##
-test_that("Different level labels, more levels, but same level order",
+test_that("Unused labels, different level labels, same level encoding",
           {
             expect_that(fct_equiv(f,g),is_true())
             expect_that(fct_equiv(g,f),is_true())  ## Commutative
@@ -86,9 +110,9 @@ test_that("Different level labels, more levels, but same level order",
 
 
 g <- factor(c(1,1,1,3,3,3,2,2,2,3,3,3,3),
-            levels = 6:1)
+            levels = 6:1,labels=LETTERS[6:1])
 ##
-test_that("More level labels, same ones _used_, different level order",
+test_that("Unused labels, same ones _used_, different level encoding",
           {
             expect_that(fct_equiv(f,g),is_true())
             expect_that(fct_equiv(g,f),is_true())  ## Commutative
@@ -115,6 +139,21 @@ test_that("One or both factors contain NAs",
             expect_that(is.na(fct_equiv(g,g)),is_true())  ## Both factors contain NA's
             expect_that(is.na(fct_equiv(f,g,ordered=TRUE)),is_true())
             expect_that(is.na(fct_equiv(g,f,ordered=TRUE)),is_true())  ## Commutative
+            expect_that(is.na(fct_equiv(g,g,ordered=TRUE)),is_true())  ## Both factors contain NA's
+          })
+
+
+g <- factor(rep(NA,length(f)),levels=1:6)
+##
+test_that("One or both factors contain only NAs",
+          {
+            expect_that(is.na(fct_equiv(f,g)),is_true())
+            expect_that(is.na(fct_equiv(g,f)),is_true())  ## Commutative
+            expect_that(is.na(fct_equiv(g,g)),is_true())  ## Both factors contain NA's
+            expect_that(is.na(fct_equiv(f,g,ordered=TRUE)),is_true())
+            expect_that(is.na(fct_equiv(g,f,ordered=TRUE)),is_true())  ## Commutative
+            expect_that(is.na(fct_equiv(g,g,ordered=TRUE)),is_true())  ## Both factors contain NA's
+
           })
 
 
