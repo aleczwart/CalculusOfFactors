f9b1d71d2b9f52904916861aa3e0b43ab06b646c
Alexander Zwart <zwa005@csiro.au>
2017-12-11 13:48:27 +1100

    Add tests for refactored warnings

--- tests/testthat/test_finer_coarser.R
+++ tests/testthat/test_finer_coarser.R
@@ -1,3 +1,4 @@
+
 library(CalculusOfFactors)
 context("Finer, coarser relationships")
 
@@ -30,6 +31,28 @@ test_that("Finer and coarser functions & operators",
             expect_that(fct_finer(as.integer(f),g),throws_error())
             ## Factor lengths must be equal:
             expect_that(fct_finer(f[1:9],g),throws_error())
+          })
+
+
+## fct_finer & fct_coarser are defined with arguments f and g.  I've
+## refactored the warning generator code to ensure that warnings are
+## given in terms of the names of factors passed to the function, not
+## simply 'f' or 'g'. Previously, warnings were always given in terms
+## of 'f' or 'g' when the warning was generated in a call to,
+## e.g. fct_equiv() from within fct_finer().
+test_that("Finer and coarser warnings",
+          {
+            x <- factor(c(1,1,2,2,3,3,4,4),levels=1:6)
+            y <- factor(c(1,1,1,1,2,3,2,2))
+            ##
+            expect_that(fct_finer(x,y),
+                        gives_warning("Factor x contains unused levels"))
+            expect_that(fct_coarser(x,y),
+                        gives_warning("Factor x contains unused levels"))
+            expect_that(x %is_finer_than% y,
+                        gives_warning("Factor x contains unused levels"))
+            expect_that(x %is_coarser_than% y,
+                        gives_warning("Factor x contains unused levels"))
           })
 
 
